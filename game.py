8a6df270150b08c4c3b7b772a74b246e4f492bf2
Anton <anton.shikalyov@gmail.com>
2016-06-15 23:29:03 +0300

    update

--- game.py
+++ game.py
@@ -1,5 +1,11 @@
 import random
+import sys
 chislo=input("Please enter the number ot 1-5!")
+if chislo in ['1','2','3','4','5']:
+	pass
+else:
+    print("not numbers")
+    sys.exit()
 chislo=int(chislo)
 vybor=random.randint(1, 5)
 if  vybor >  chislo:
